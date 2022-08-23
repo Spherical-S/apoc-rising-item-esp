58f5e48b2a2c27897f1f524744bc0ec2277fe930
Spherical-S <sadiq.shahid101@gmail.com>
2022-08-23 15:45:54 -0400

    d

--- esp.lua
+++ esp.lua
@@ -26,16 +26,47 @@ end
 
 uIS = game:GetService("UserInputService")
 RunService = game:GetService("RunService")
-lKey = Enum.KeyCode.L
+uKey = Enum.KeyCode.U
+iKey = Enum.KeyCode.I
+oKey = Enum.KeyCode.O
 player = game:GetService("Players").LocalPlayer
 typing = false
-espOn = true
+wEspOn = true
+aEspOn = true
+vEspOn = true
 weapons = getWeapons()
 ammo = getAmmo()
 
 clearESP()
-for i,v in pairs(game.Workspace.Loot:GetDescendants()) do
-    if weapons[v.Name] or ammo[v.Name] then
-        print(v.Name)
+-- for i,v in pairs(game.Workspace.Loot:GetDescendants()) do
+--     if weapons[v.Name] or ammo[v.Name] then
+--         print(v.Name)
+--     end
+-- end
+
+uIS.TextBoxFocused:Connect(function()
+    typing = true
+end)
+
+uIS.TextBoxFocusReleased:Connect(function()
+    typing = false
+end)
+
+uIS.InputBegan:Connect(function(Input)
+    if Input.KeyCode == iKey and typing == false then
+        wEspOn = not wEspOn
     end
-end
+    if Input.KeyCode == oKey and typing == false then
+        aEspOn = not aEspOn
+    end
+    if Input.KeyCode == uKey and typing == false then
+        vEspOn = not vEspOn
+    end
+end)
+
+print('Done initializing')
+
+local success, response = pcall(
+    createESP()
+)
+print('ESP loaded!')
